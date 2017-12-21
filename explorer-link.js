621e55e09ae06faa73fca15fb3d093c083749de0
fmrsabino <fredericomsabino@gmail.com>
2017-12-21 12:41:45 +0100

    Use https instead of http

--- explorer-link.js
+++ explorer-link.js
@@ -2,5 +2,5 @@ const prefixForNetwork = require('./prefix-for-network')
 
 module.exports = function (hash, network) {
   const prefix = prefixForNetwork(network)
-  return `http://${prefix}etherscan.io/tx/${hash}`
+  return `https://${prefix}etherscan.io/tx/${hash}`
 }
