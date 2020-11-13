e08986527a73ee87b93ac2344cdebd8ef31b9e09
Alexander Cucer <alexander.moldova@gmail.com>
2020-11-13 17:44:17 +0200

    Export token tracker link

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import createExplorerLink from './explorer-link'
 import createAccountLink from './account-link'
+import createTokenTrackerLink './token-tracker-link'
 
 export = {
   createExplorerLink,
-  createAccountLink
+  createAccountLink,
+  createTokenTrackerLink
 }
