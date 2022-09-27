26b31d88f5d592a9748ee47efb07f2bf250cad8d
ryanml <ryanlanese@gmail.com>
2022-09-27 09:15:43 -0700

    Adding Sepolia network to network prefixes (#64)

--- src/prefix-for-network.ts
+++ src/prefix-for-network.ts
@@ -10,6 +10,9 @@ export = function getPrefixForNetwork(networkId: string): string | null {
     case 5: // goerli test net
       prefix = 'goerli.';
       break;
+    case 11155111: // sepolia test net
+      prefix = 'sepolia.';
+      break;
     default:
       prefix = null;
   }
