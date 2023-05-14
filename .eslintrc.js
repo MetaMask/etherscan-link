1e4d5f249b103b17b3e657df5eea5ef33ccf3d4e
legobt <6wbvkn0j@anonaddy.me>
2023-05-14 21:53:35 +0000

    lint: temporarily disable jsdoc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,12 @@ module.exports = {
   root: true,
   extends: ['@metamask/eslint-config', '@metamask/eslint-config-mocha'],
   ignorePatterns: ['!.eslintrc.js', 'dist/'],
+  rules: {
+    // TODO: Fix jsdoc comments and enable rules
+    'jsdoc/require-description': 'off',
+    'jsdoc/require-param-description': 'off',
+    'jsdoc/require-returns': 'off',
+  },
   overrides: [
     {
       extends: ['@metamask/eslint-config-nodejs'],
